cc08b5c623a3d186f9cdf8376171116ed5f49b80
sudo180272 <sudo180272@protonmail.ch>
2021-02-17 02:21:30 +0900

    add App, CalendarScreen, HomeScreen, SettingsScreen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './components/HomeScreen';
+import CalendarScreen from './components/CalendarScreen';
+import SettingsScreen from './components/SettingsScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+
+const Tab = createMaterialBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="CalendarScreen" component={CalendarScreen} />
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
