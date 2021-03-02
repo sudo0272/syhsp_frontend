642bef0bea3a42b8057f000821a86db2dd602e31
sudo180272 <sudo180272@protonmail.ch>
2021-03-02 22:09:13 +0900

    interfaces: merged with classes

--- interfaces/HomeLinkButtonProps.ts
+++
@@ -1,7 +0,0 @@
-export default interface HomeLinkButtonProps {
-  icon: string;
-  title: string;
-  uri: string;
-  backgroundColor: string;
-}
-
--- interfaces/VolunteerHomeLinkButtonProps.ts
+++
@@ -1,5 +0,0 @@
-export default interface VolunteerHomeLinkButtonProps {
-  title: string;
-  uri: string;
-}
-
