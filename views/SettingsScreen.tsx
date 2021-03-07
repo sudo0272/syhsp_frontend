010a666d210667afc17962190a3691ec8a843eb7
sudo180272 <sudo180272@protonmail.ch>
2021-03-07 22:45:12 +0900

    Profile: implement

--- views/SettingsScreen.tsx
+++ views/SettingsScreen.tsx
@@ -1,13 +1,29 @@
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import React, {
+  Component
+} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet
+} from 'react-native';
+import AccountDatat from '../models/AccountData';
+import Profile from './Profile';
 
 export default class SettingsScreen extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <Text>Settings</Text>
+      <View style={styles.container}>
+        <Profile
+        />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column"
+  }
+});
 
